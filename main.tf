49927bc8ea899ea2cde5ecb2d5f6df105c6aff01
mashalt07 <maltamash2880@gmail.com>
2025-02-18 11:22:52 +0000

    testing terraform plan

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket = "maltamash-tf-state-bucket"
+    key = "statefile"
+    region = "eu-west-2"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "eu-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d26eb3972b7f8c96"
+  instance_type = "t2.micro"
+}
